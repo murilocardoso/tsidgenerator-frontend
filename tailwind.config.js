5b4712824ed920bf15d0fcfc689aeec497523ca3
Murilo Carlos Cardoso <murilo.carlos@totvs.com.br>
2024-07-30 10:30:45 -0300

    tsid generator lives

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  content: [
+    './public/index.html',
+    './src/**/*.{html,js}',
+  ],
+  // ...
 }
-
